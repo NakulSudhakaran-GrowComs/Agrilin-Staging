080c71a1921af8cbc9ff0cb3c15ee697b458147f
admin <it@growcoms.com>
2024-06-14 09:52:54 +0530

    Route changed

--- agrilin/src/test/java/com/agrilin/generic/IAutoConst.java
+++ agrilin/src/test/java/com/agrilin/generic/IAutoConst.java
@@ -7,9 +7,11 @@ public interface IAutoConst {
 	String CHROME_VALUE="D:\\Eclipse\\eclipse-workspace\\utility\\driver\\chromedriver.exe";
 	String FIREFOX_KEY="webdriver.gecko.driver";
 	String FIREFOX_VALUE="D:\\Eclipse\\eclipse-workspace\\utility\\driver\\geckodriver.exe";
-	String SETTINGS_PATH="D:\\Eclipse\\eclipse-workspace\\utility\\settings.properties";
+	//String SETTINGS_PATH="D:\\Eclipse\\eclipse-workspace\\utility\\settings.properties";
+	String SETTINGS_PATH="D:\\Agrilin-Staging\\utility\\settings.properties";
 	String OR_SETTINGS_PATH="D:\\Eclipse\\eclipse-workspace\\agrilin\\src\\test\\resources\\properties\\ObjectRepository.properties";
-	String SCREENSHOTS_FOLDER_PATH = PROJECT_DERECTORY+"\\screenshots\\";
+	String SCREENSHOTS_FOLDER_PATH = "D:\\Agrilin-Staging\\agrilin\\screenshots";
 	String EXCEL_SHEET_PATH=PROJECT_DERECTORY+"\\excel\\AgrilinWorkBook.xlsx";
-	String EXTENT_REPORT_CREDENTIALS_SETTINGS_PATH="D:\\Eclipse\\eclipse-workspace\\utility\\extentreportcredentials.properties";
+	//String EXTENT_REPORT_CREDENTIALS_SETTINGS_PATH="D:\\Eclipse\\eclipse-workspace\\utility\\extentreportcredentials.properties";
+	String EXTENT_REPORT_CREDENTIALS_SETTINGS_PATH="D:\\Agrilin-Staging\\utility\\extentreportcredentials.properties";
 }
